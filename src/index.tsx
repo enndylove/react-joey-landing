49d6484b552f7ff816760f20b66ccedd4bc2457c
enndy <91780685+enndylove@users.noreply.github.com>
2024-10-05 02:56:14 +0300

    add main scss style

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './Home';
 
+import './styles/_main.scss'
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
