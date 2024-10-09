4c61a561d1941b3016f4be54c3b4657ba158f5c5
enndy <91780685+enndylove@users.noreply.github.com>
2024-10-09 04:26:54 +0300

    remove wow classname and check active class in burger menu

--- src/components/Header/assets/Theme.tsx
+++ src/components/Header/assets/Theme.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 // @ts-ignore
 import IconTheme from '../../../images/icons/theme.svg'
 
-export default function Theme() {
+export default function Theme({ onClick, burgerActive }: any) {
     return (
             <div
-                className={`header__theme cursor-pointer wow animate__animated animate__fadeIn`}
+                className={`header__theme cursor-pointer animate__animated animate__fadeIn ${burgerActive ? 'active' : ''}`}
                 style={{animationDelay: "0.35s"}}
+                onClick={onClick}
             >
                 <img
                     className={`header__theme-icon`}
